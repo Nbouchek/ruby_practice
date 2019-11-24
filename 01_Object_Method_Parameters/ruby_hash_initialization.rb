6b7316ef915b8c5f322078c9f0961fda48f15efa
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-24 12:45:15 +0000

    Hash initialization

---
+++ 01_Object_Method_Parameters/ruby_hash_initialization.rb
@@ -0,0 +1,17 @@
+# Initialize an empty Hash with the variable name empty_hash
+empty_hash = Hash.new
+
+# Initialize an empty Hash with the variable name default_hash and the default value of every key set to 1.
+default_hash = Hash.new(1)
+
+# This would also work
+# default_hash = Hash.new
+# default_hash.default = 1
+
+# Initialize a hash with the variable name hackerrank and having the key-value pairs
+hackerrank = {"simmy" => 100, "vivmbbs" => 200}
+
+# or
+ # hackerrank = Hash.new
+# hackerrank["simmy"] = 100
+# hackerrank["vivmbbs"] = 200
